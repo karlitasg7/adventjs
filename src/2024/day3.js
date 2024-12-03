9a59c1ba1534072ae5a342003ac55d5a254addf7
Karla Sequen <karlasequen@gmail.com>
2024-12-02 20:16:16 -0600

    add 2024 day 3 - implement inventory organization function with tests

---
+++ src/2024/day3.js
@@ -0,0 +1,12 @@
+function organizeInventory(inventory) {
+  const result = {};
+
+  for (const { category, name, quantity } of inventory) {
+    result[category] = result[category] || {};
+    result[category][name] = (result[category][name] || 0) + quantity;
+  }
+
+  return result;
+}
+
+module.exports = { organizeInventory };
---
+++ tests/2024/day3.test.js
@@ -0,0 +1,45 @@
+const { organizeInventory } = require("../../src/2024/day3");
+
+describe("Day 03 - Organizing the inventory", () => {
+  test("return empty if array is empty", () => {
+    expect(organizeInventory([])).toEqual({});
+  });
+
+  test('test 1"', () => {
+    expect(
+      organizeInventory([
+        { name: "doll", quantity: 5, category: "toys" },
+        { name: "car", quantity: 3, category: "toys" },
+        { name: "ball", quantity: 2, category: "sports" },
+        { name: "car", quantity: 2, category: "toys" },
+        { name: "racket", quantity: 4, category: "sports" },
+      ])
+    ).toEqual({
+      toys: {
+        doll: 5,
+        car: 5,
+      },
+      sports: {
+        ball: 2,
+        racket: 4,
+      },
+    });
+  });
+
+  test("test 2", () => {
+    expect(
+      organizeInventory([
+        { name: "book", quantity: 10, category: "education" },
+        { name: "book", quantity: 5, category: "education" },
+        { name: "paint", quantity: 3, category: "art" },
+      ])
+    ).toEqual({
+      education: {
+        book: 15,
+      },
+      art: {
+        paint: 3,
+      },
+    });
+  });
+});
