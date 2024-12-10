5c338158a08369adcde5632eaf683875891f9307
Karla Sequen <karlasequen@gmail.com>
2024-12-09 21:35:18 -0600

    refactor day 6 solution to simplify box boundary checks

--- src/2024/day6/solution.js
+++ src/2024/day6/solution.js
@@ -4,15 +4,12 @@ function inBox(box) {
 
   for (let row = 1; row < rows - 1; row++) {
     const col = box[row].indexOf("*");
-    if (col > 0 && col < cols - 1) {
-      if (
-        box[0][col] === "#" &&
-        box[rows - 1][col] === "#" &&
-        box[row][0] === "#" &&
-        box[row][cols - 1] === "#"
-      ) {
-        return true;
-      }
+    if (
+      box[row].includes("*") &&
+      box[row][0] === "#" &&
+      box[row][cols - 1] === "#"
+    ) {
+      return true;
     }
   }
 
