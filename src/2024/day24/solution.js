f880ebc717b6dac5079ba32f14203c1ab8b50297
Karla Sequen <karlasequen@gmail.com>
2024-12-26 10:25:47 -0600

    improve day 24

--- src/2024/day24/solution.js
+++ src/2024/day24/solution.js
@@ -1,16 +1,15 @@
 function isTreesSynchronized(tree1, tree2) {
-  const areNodesMirrored = (node1, node2) => {
-    if (!node1 && !node2) return true;
-    if (!node1 || !node2 || node1.value !== node2.value) return false;
-    return (
-      areNodesMirrored(node1.left, node2.right) &&
-      areNodesMirrored(node1.right, node2.left)
-    );
-  };
+  function traverse(node1, node2) {
+    if (!node1 || !node2) return true;
 
-  const synchronized = areNodesMirrored(tree1, tree2);
+    let result = true;
+    result &&= traverse(node1.right, node2.left);
+    result &&= traverse(node1.left, node2.right);
 
-  return [synchronized, tree1?.value];
+    return result && node1.value === node2.value;
+  }
+
+  return [traverse(tree1, tree2), tree1.value];
 }
 
 module.exports = { isTreesSynchronized };
